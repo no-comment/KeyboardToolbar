30eba4db4b4fc4219df653c2ee74faef25050d8a
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2021-03-10 23:32:08 +0100

    Hide toolbar if no items provided (Improved)

--- Sources/KeyboardToolbar/ViewExtension.swift
+++ Sources/KeyboardToolbar/ViewExtension.swift
@@ -8,15 +8,17 @@ internal struct AppendKeyboardToolbar: ViewModifier {
     func body(content: Content) -> some View {
         ZStack {
             content
-                .padding(.bottom, responder.currentHeight == 0 ? 0 : style.height)
+                .padding(.bottom, (responder.currentHeight == 0 || items.isEmpty) ? 0 : style.height)
             
             VStack(spacing: 0) {
                 Spacer()
                 
-                Divider()
-                    .foregroundColor(style.dividerColor)
-                
-                KeyboardToolbar(items: items, style: style)
+                if !items.isEmpty {
+                    Divider()
+                        .foregroundColor(style.dividerColor)
+                    
+                    KeyboardToolbar(items: items, style: style)
+                }
             }
             .opacity(responder.currentHeight == 0 ? 0 : 1)
             .animation(.easeOut(duration: responder.duration))
@@ -29,10 +31,6 @@ internal struct AppendKeyboardToolbar: ViewModifier {
 
 public extension View {
     func keyboardToolbar(_ items: [KeyboardToolbarItem], style: KeyboardToolbarStyle = .standard) -> some View {
-        if items.isEmpty {
-            return AnyView(self)
-        } else {
-            return AnyView(self.modifier(AppendKeyboardToolbar(items: items, style: style)))
-        }
+        self.modifier(AppendKeyboardToolbar(items: items, style: style))
     }
 }
