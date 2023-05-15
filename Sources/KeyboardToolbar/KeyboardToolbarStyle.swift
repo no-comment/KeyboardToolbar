fcd016a68b4cc2d66efb3583d12abbaaaa827570
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2023-05-15 10:43:52 +0200

    Add connivence functions

--- Sources/KeyboardToolbar/KeyboardToolbarStyle.swift
+++ Sources/KeyboardToolbar/KeyboardToolbarStyle.swift
@@ -29,6 +29,33 @@ public struct KeyboardToolbarStyle {
     }
 }
 
+public extension KeyboardToolbarStyle {
+    func withBackgroundColor(_ color: Color = Color(UIColor.systemGray4)) -> Self {
+        return .init(backgroundColor: color, height: self.height, itemSize: self.itemSize, itemSpacing: self.itemSpacing, dividerColor: self.dividerColor, dividerWidth: self.dividerWidth, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withHeight(_ h: CGFloat = 40) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: h, itemSize: self.itemSize, itemSpacing: self.itemSpacing, dividerColor: self.dividerColor, dividerWidth: self.dividerWidth, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withItemSize(_ s: CGFloat = 20) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: self.height, itemSize: s, itemSpacing: self.itemSpacing, dividerColor: self.dividerColor, dividerWidth: self.dividerWidth, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withItemSpacing(_ s: CGFloat = 10) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: self.height, itemSize: self.itemSize, itemSpacing: s, dividerColor: self.dividerColor, dividerWidth: self.dividerWidth, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withDividerColor(_ c: Color = .gray) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: self.height, itemSize: self.itemSize, itemSpacing: self.itemSpacing, dividerColor: c, dividerWidth: self.dividerWidth, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withDividerWidth(_ w: CGFloat = 0.5) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: self.height, itemSize: self.itemSize, itemSpacing: self.itemSpacing, dividerColor: self.dividerColor, dividerWidth: w, dividerBetweenItems: self.dividerBetweenItems)
+    }
+
+    func withDividerBetweenItems(_ b: Bool = false) -> Self {
+        return .init(backgroundColor: self.backgroundColor, height: self.height, itemSize: self.itemSize, itemSpacing: self.itemSpacing, dividerColor: self.dividerColor, dividerWidth: self.dividerWidth, dividerBetweenItems: b)
     }
 }
 
